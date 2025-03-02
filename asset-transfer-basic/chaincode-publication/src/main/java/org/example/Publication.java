1d094a964aace29611d680f21d85883159d230a2
Rui Martins <ruipedro200205@gmail.com>
2025-03-02 16:44:46 +0000

    started Publication smartcontract

---
+++ asset-transfer-basic/chaincode-publication/src/main/java/org/example/Publication.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import com.owlike.genson.annotation.JsonProperty;
+import java.util.Objects;
+import org.hyperledger.fabric.contract.annotation.DataType;
+import org.hyperledger.fabric.contract.annotation.Property;
+
+@DataType
+public class Publication {
+
+    @Property
+    private final String id;
+
+    @Property
+    private final String title;
+
+    public Publication(
+            @JsonProperty("id") final String id,
+            @JsonProperty("title") final String title
+    ) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Publication that = (Publication) o;
+        return Objects.equals(id, that.id) && Objects.equals(title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+}
---
+++ asset-transfer-basic/chaincode-publication/src/main/java/org/example/PublicationService.java
@@ -0,0 +1,60 @@
+package org.example;
+
+
+import com.owlike.genson.Genson;
+import java.util.ArrayList;
+import java.util.List;
+import org.hyperledger.fabric.contract.Context;
+import org.hyperledger.fabric.contract.ContractInterface;
+import org.hyperledger.fabric.contract.annotation.Contract;
+import org.hyperledger.fabric.contract.annotation.Default;
+import org.hyperledger.fabric.contract.annotation.Transaction;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+import org.hyperledger.fabric.shim.ledger.KeyValue;
+import org.hyperledger.fabric.shim.ledger.QueryResultsIterator;
+
+@Contract(name = "basic")
+@Default
+public class PublicationService implements ContractInterface {
+
+    private final Genson genson = new Genson();
+
+    @Transaction(intent = Transaction.TYPE.SUBMIT)
+    public void InitLedgerPublications(final Context ctx) {
+        put(ctx, new Publication("1", "publication1"));
+        put(ctx, new Publication("2", "publication2"));
+    }
+
+
+    @Transaction(intent = Transaction.TYPE.EVALUATE)
+    public String GetAllPublications(final Context ctx) {
+        ChaincodeStub stub = ctx.getStub();
+
+        List<Publication> queryResults = new ArrayList<>();
+
+        // To retrieve all assets from the ledger use getStateByRange with empty startKey & endKey.
+        // Giving empty startKey & endKey is interpreted as all the keys from beginning to end.
+        // As another example, if you use startKey = 'asset0', endKey = 'asset9' ,
+        // then getStateByRange will retrieve asset with keys between asset0 (inclusive) and asset9 (exclusive) in lexical order.
+        QueryResultsIterator<KeyValue> results = stub.getStateByRange("", "");
+
+        for (KeyValue result: results) {
+            Publication publication = genson.deserialize(result.getStringValue(), Publication.class);
+            System.out.println(publication);
+            queryResults.add(publication);
+        }
+
+        return genson.serialize(queryResults);
+    }
+
+    private Publication put(final Context ctx, final Publication publication) {
+        // Use Genson to convert the Asset into string, sort it alphabetically and serialize it into a json string
+        String sortedJson = genson.serialize(publication);
+        ctx.getStub().putStringState(publication.getId(), sortedJson);
+
+        return publication;
+    }
+
+
+
+}
