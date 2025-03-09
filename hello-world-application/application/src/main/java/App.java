ba243328b95d1ad3b62b570c980204036d239a9a
Rui Martins <ruipedro200205@gmail.com>
2025-03-09 12:18:47 +0000

    changed name of packages

--- hello-world-application/application/src/main/java/App.java
+++ hello-world-application/application/src/main/java/App.java
@@ -1,9 +1,3 @@
-/*
- * Copyright IBM Corp. All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
